0d142d1a1b920aae92103c2778649d611579df1f
GustavoMarizA <100234598+GustavoMarizA@users.noreply.github.com>
2024-04-03 17:16:08 -0300

    Create Mesa.java

---
+++ codigo/pao-de-queijo/src/Mesa.java
@@ -0,0 +1,15 @@
+/**
+ * Representa uma mesa no restaurante
+*/
+public class Mesa{
+    public int numero;
+    private int capacidade;
+    private boolean disponivel;
+
+    /**
+     * Construtor da classe Mesa.
+     * @param numero Numero da mesa
+     * @param capacidade Capacidade da mesa
+     * @param disponivel Mostra se a mesa esta disponivel (diponivel = true) ou não (disponivel = false)
+    */
+}
